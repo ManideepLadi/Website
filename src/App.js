c3b2c0e7f7aad3c7c6140a7d965889466a4c1d8d
manideep <manideep.ladi@zemosolabs.com>
2020-08-25 21:36:59 +0530

    Adding homepage

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+const styling={
+  backgroundStyle : {
+    background: 'no-repeat center right fixed url(manideepSketch.jpeg)',
+    marginBottom: 0,
+    padding: 0
+  },
+  container :{
+    position: 'relative',
+    top: '28%',
+    zIndex: 4,
+    textAlign: 'center'
+  },
+  // overlay : {
+  //     background-color: deepskyblue;position: absolute;top: 0;bottom: 0;left: 0;right: 0;opacity: 0.75;filter:alpha(opacity=75);z-index: 2;
+  // }
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div id="top" style={styling.backgroundStyle} data-src="/view/images/tower_scope_animation.gif" data-position="center right">
+        <div style={styling.container}>
+          <h1>Manideep Ladi</h1>
+          <p>Interactive resume</p>
+        </div>
+
+        {/*<div className="overlay"></div>*/}
+      </div>
+
   );
 }
 
